f895ce87ca3508ce7ced9553577e291a6a13c59c
Mohit Thakur <153229801+MohitThakurr@users.noreply.github.com>
2024-08-28 23:33:46 +0530

    Update Data Cleaning Main.py

--- Data Cleaning Main.py
+++ Data Cleaning Main.py
@@ -4,7 +4,7 @@ import numpy as np
 class DataCleaner:
     def __init__(self, file_path):
         self.data = pd.read_csv(file_path)
-        #Self.git update Filee
+        #Self.git update Fileee
     
     def handle_missing_values(self, strategy='mean', fill_value=None, columns=None):
         if columns is None:
